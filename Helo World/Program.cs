bda492fd1a7a2e1470fce9a36507d0309abe7c80
Andrew Tieszen <pilottzn@gmail.com>
2023-09-07 18:11:40 -0600

    feat: Created main class prints "Hello World" to console BREAKING CHANGE

--- Helo World/Program.cs
+++ Helo World/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
